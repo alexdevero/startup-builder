0e7dd98d0f9b328b75e2bbeee36c49d4303c5638
Alex Devero <deveroalex@gmail.com>
2019-05-20 14:26:43 +0200

    Refactor item & column components for board build

--- src/components/board-item.tsx
+++ src/components/board-item.tsx
@@ -2,34 +2,41 @@ import * as React from 'react'
 import { Draggable } from 'react-beautiful-dnd'
 import styled from 'styled-components'
 
-type ColumnItemProps = {
-  index: any,
+type BoardItemProps = {
+  index: number
   item: any
 }
 
-type ColumnItemElProps = {
+type BoardItemElProps = {
   isDragging: boolean
 }
 
-const ColumnItemEl = styled.div<ColumnItemElProps>`
-  background-color: ${(props) => props.isDragging ? '#eee' : '#fff'};
+const BoardItemEl = styled.div<BoardItemElProps>`
+  padding: 8px;
+  background-color: ${(props) => props.isDragging ? '#d3e4ee' : '#fff'};
+  border-radius: 4px;
+  transition: background-color .25s ease-out;
+
+  &:hover {
+    background-color: #f7fafc;
+  }
 
   & + & {
     margin-top: 4px;
   }
 `
 
-export const ColumnItem: React.FC<ColumnItemProps> = (props) => {
+export const BoardItem = (props: BoardItemProps) => {
   return <Draggable draggableId={props.item.id} index={props.index}>
     {(provided, snapshot) => (
-      <ColumnItemEl
+      <BoardItemEl
         {...provided.draggableProps}
         {...provided.dragHandleProps}
         ref={provided.innerRef}
         isDragging={snapshot.isDragging}
       >
         {props.item.content}
-      </ColumnItemEl>
+      </BoardItemEl>
     )}
   </Draggable>
 }
